c8d0ede565f0df7bae25d0a998c0b93201b50166
hzeze <1150449493@qq.com>
2025-12-15 23:13:06 +0800

    后端初始化

---
+++ src/test/java/com/hz/aiagent/AiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hz.aiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
